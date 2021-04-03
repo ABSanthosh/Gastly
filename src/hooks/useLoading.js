869e54a9713954c045b9c63bd597cdbb08bb28c3
ABSanthosh <a.b.santhosh02@gmail.com>
2021-04-04 01:03:57 +0530

    Loading hook refactoring and root overflow limiting

--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -1,20 +1,23 @@
-import ReactDOM from 'react-dom';
-import LoadingBoxes from '../components/LoadingBoxes/LadingBoxes';
+import LoadingBoxes from "../components/LoadingBoxes/LadingBoxes";
+import ReactDOM from "react-dom";
+import { overflow } from "../Util/overflow";
 
 export function useLoading() {
   const startLoading = () => {
+    document.getElementById("root").style.overflow = "hidden";
     document
       .querySelector(".Maincontainer__backdrop")
-      .insertAdjacentHTML('afterend', '<div id="loading-dock"></div>');
-    ReactDOM.render(<LoadingBoxes />, document.getElementById('loading-dock'));
+      .insertAdjacentHTML("afterend", '<div id="loading-dock"></div>');
+    ReactDOM.render(<LoadingBoxes />, document.getElementById("loading-dock"));
   };
 
   const stopLoading = () => {
-    document.getElementById('loading-dock').remove();
+    document.getElementById("root").style.overflow = "unset";
+    document.getElementById("loading-dock").remove();
   };
 
   return {
     startLoading,
-    stopLoading
+    stopLoading,
   };
 }
