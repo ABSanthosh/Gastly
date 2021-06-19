39b3009163db4b9b6a40bd95440d38d6d1f4a372
ABSanthosh <a.b.santhosh02@gmail.com>
2021-06-19 18:03:05 +0530

    Pokemon cries play component

---
+++ src/components/Cries/Cries.js
@@ -0,0 +1,48 @@
+import "./Cries.scss";
+import Play from "../../Assets/Images/play.png";
+import Pause from "../../Assets/Images/pause.png";
+import { forifier } from "../../Util/forifier";
+
+function Cries({ pokeId }) {
+  return (
+    <>
+      <audio
+        id="PokemonCry"
+        src={`https://gastly-dex.github.io/PokedexData/PokemonCries/${forifier(
+          pokeId
+        )}.mp3`}
+        onError={() => {
+          document.getElementById("audioBoxImg").style.opacity = 0.5;
+          document.getElementById("audioBoxImg").style.cursor = "unset"
+        }}
+        onLoad={()=>{
+          document.getElementById("audioBoxImg").style.opacity = 1;
+          document.getElementById("audioBoxImg").style.cursor = "pointer"
+        }}
+      />
+      <img
+        className="audioBoxImg"
+        id="audioBoxImg"
+        onClick={() => {
+          const audioElement = document.getElementById("PokemonCry");
+          if (
+            document.getElementById("audioBoxImg").src == Play &&
+            document.getElementById("audioBoxImg").style.opacity != 0.5
+          ) {
+            document.getElementById("audioBoxImg").src = Pause;
+            audioElement.play();
+            setTimeout(() => {
+              document.getElementById("audioBoxImg").src = Play;
+            }, audioElement.duration * 1000 - audioElement.currentTime * 1000);
+          } else {
+            document.getElementById("audioBoxImg").src = Play;
+            audioElement.pause();
+          }
+        }}
+        src={Play}
+      ></img>
+    </>
+  );
+}
+
+export default Cries;
