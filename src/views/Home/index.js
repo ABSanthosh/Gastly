4a35b302c3a0ac3b8c6495ab23294f9dd37a34c0
ABSanthosh <a.b.santhosh02@gmail.com>
2021-06-20 20:37:54 +0530

    Dark mode option added (Finally!)

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -9,7 +9,6 @@ import Box from "../../components/Box/Box";
 import HintBox from "./components/HintBox/HintBox";
 import { InitialConditions } from "../../Util/SpriteConditions";
 import NewSprites from "../../Assets/FromOldJson.json";
-
 import PokeSprite from "./components/PokeSprite/PokeSprite";
 import SpriteVariation from "./components/SpriteVariation/SpriteVariation";
 import { SpriteVariationControlPanel } from "../../Util/SpriteVariationCP";
@@ -21,6 +20,8 @@ import EvalChainItem from "../../components/EvalChainItem/EvalChainItem";
 import Cries from "../../components/Cries/Cries";
 import Feet from "../../components/Feet/Feet";
 import Tabs from "./components/Tabs/Tabs";
+import DarkMode from "../../Assets/Images/DarkMode.gif";
+import LightMode from "../../Assets/Images/LightMode.gif";
 
 function Home() {
   let { id } = useParams();
@@ -59,6 +60,8 @@ function Home() {
   const [stats, setStats] = useState([0, 0, 0, 0, 0, 0]);
   const [poketypes, setPoketypes] = useState();
   const [pokeabilities, setPokeabilities] = useState();
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("Theme")));
+
   let poketype, pokeability, evalChainComponent;
 
   useEffect(() => {
@@ -114,8 +117,12 @@ function Home() {
     SpriteVariationControlPanel(iconFocus, id, setUrl);
   }, [iconFocus]);
 
+  useEffect(() => {
+    localStorage.setItem("Theme", JSON.stringify(theme));
+  }, [theme]);
+
   return (
-    <div className="Maincontainer">
+    <div className={`Maincontainer ${theme}`}>
       <div className="Maincontainer__contentwrapper">
         <div className="content__backdrop">
           <Backdrop isMobile={false} fill={backdropcolor} />
@@ -123,7 +130,19 @@ function Home() {
         <div className="content__search">
           <div className="content__SearchBarBox">
             <span className="fa fa-search" />
-            <div className="content__pokemonName">{pokename}</div>
+            <div className="content__pokemonName">
+              {pokename}
+              <img
+                className="content__themeToggle"
+                onClick={() => {
+                  console.log("Hi");
+                  theme == "LightMode"
+                    ? setTheme("DarkMode")
+                    : setTheme("LightMode");
+                }}
+                src={theme == "LightMode" ? DarkMode : LightMode}
+              />
+            </div>
             <HintBox text={text} setText={setText} />
           </div>
         </div>
@@ -257,70 +276,3 @@ function Home() {
 }
 
 export default Home;
-
-// <div className="Maincontainer__backdrop">
-//          <Backdrop isMobile={false} fill={backdropcolor} />
-//        </div>
-//         <div className="Maincontainer__content">
-// <div className="content__image">
-// <Backdrop isMobile={true} fill={backdropcolor} />
-// <div
-//   className="leftChevron"
-//   onClick={() => {
-//     history.push("/" + String(parseInt(id) - 1));
-//     startLoading();
-//   }}
-// />
-// <PokeSprite
-//   imgRef={imgRef}
-//   Url={Url}
-//   setColor={setColor}
-//   NewSprites={NewSprites}
-//   forifier={forifier}
-//   id={id}
-// />
-// {/* <ImgLoading/> */}
-// <div
-//   className="rightChevron"
-//   onClick={() => {
-//     history.push("/" + String(parseInt(id) + 1));
-//     startLoading();
-//   }}
-// />
-//           </div>
-//           <div className="content__details">
-// <div className="content__SearchBarBox">
-//   <span className="fa fa-search" />
-//   <div className="content__pokemonName">{pokename}</div>
-//   <HintBox text={text} setText={setText} />
-// </div>
-//             <div className="content__contentwrapper">
-// <Box Height="100px" className="content__desc">
-//   <p>{desc}</p>
-// </Box>
-// <div className="content__row">
-//   <Box Width="100%" padding="13px 0px">
-//     {poketypes}
-//   </Box>
-//   <Box Width="100%" padding="13px 0px">
-//     <SpriteVariation
-//       iconFocus={iconFocus}
-//       seticonFocus={seticonFocus}
-//       id={id}
-//     />
-//   </Box>
-// </div>
-// {/* <Box padding="11.5px" style={{ "flexDirection": "column" }}>
-//   <BaseStats className="HP" text={`HP ${stats[0]}`} percent={stats[0]} />
-//   <BaseStats className="Attack" text={`Attack ${stats[1]}`} percent={stats[1]} />
-//   <BaseStats className="Defense" text={`Defense ${stats[2]}`} percent={stats[2]} />
-//   <BaseStats className="SpAttack" text={`Sp. Attack ${stats[3]}`} percent={stats[3]} />
-//   <BaseStats className="SpDefense" text={`Sp. Defense ${stats[4]}`} percent={stats[4]} />
-//   <BaseStats className="Speed" text={`Speed ${stats[5]}`} percent={stats[5]} />
-// </Box> */}
-// <Box padding="11.5px" heading="Abilities">
-//   {pokeabilities}
-// </Box>
-//             </div>
-//           </div>
-//         </div>
