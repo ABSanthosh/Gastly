750af1cac180795372f01ead249337a2d98866ac
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-02 20:28:33 +0530

    refactor(UI): Basic structure

--- pages/[pokeId].js
+++ pages/[pokeId].js
@@ -1,8 +1,25 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 import moduleStyle from "../styles/Home.module.scss";
 
 export default function Home() {
   const router = useRouter();
   const { pokeId } = router.query;
-  return <div style={moduleStyle.homeWrapper}>{pokeId}</div>;
+  return (
+    <>
+      <Head>
+        <title>Gastly | {pokeId}</title>
+      </Head>
+      <div className={`${moduleStyle.MainContainer} LightMode`}>
+        <div className={moduleStyle.MainContainer__ContentWrapper}>
+          <div className={moduleStyle.MainContainer__japaneseName}></div>
+
+          <div className={moduleStyle.MainContainer__backdrop}></div>
+          <div className={moduleStyle.MainContainer__search}></div>
+          <div className={moduleStyle.MainContainer__pokeImage}></div>
+          <div className={moduleStyle.MainContainer__details}></div>
+        </div>
+      </div>
+    </>
+  );
 }
