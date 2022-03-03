4bd7073c3379451d88dcb8f8739cac49e1e026d3
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-03 13:36:22 +0530

    refactor(Sprite): Cross origin for color thief

--- Components/PokeSprite/PokeSprite.js
+++ Components/PokeSprite/PokeSprite.js
@@ -1,10 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import style from "./PokeSprite.module.scss";
+import ColorThief from "colorthief";
+import { useRef } from "react";
+import { rgbToHex } from "../../Utils/rgbToHex";
 
-function PokeSprite({ url }) {
+function PokeSprite({ url, setBackdropColor }) {
+  const imgRef = useRef();
   return (
-    <img alt={"Pokemon"} className={style.PokeSpriteWrapper} src={url}></img>
+    <>
+      <link rel="prefetch" as="image" href={url} crossOrigin="anonymous" />
+
+      <img
+        alt={"Pokemon"}
+        ref={imgRef}
+        crossOrigin="projectpokemon.org"
+        onLoad={() => {
+          const colorThief = new ColorThief();
+          const img = imgRef.current;
+          setBackdropColor(
+            rgbToHex(
+              colorThief.getColor(img)[0],
+              colorThief.getColor(img)[1],
+              colorThief.getColor(img)[2]
+            )
+          );
+        }}
+        className={style.PokeSpriteWrapper}
+        src={url}
+      />
+    </>
   );
 }
 
