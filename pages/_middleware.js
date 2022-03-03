6e9b6193950ebca6ea42e79d51103c13ff967856
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-03 18:53:44 +0530

    refactor(CleanUp): Cleaned some code

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req, ev) {
   const { pathname } = req.nextUrl;
-  console.log(`${pathname} middleware`);
-
   if (pathname == "/") {
     return NextResponse.redirect(
       new URL(`/${Math.floor(Math.random() * 898 + 1)}`, req.url)
