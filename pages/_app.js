ebcc9cad1fbccc27b2f220c3e868f70fcee5fbdb
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-03 13:35:28 +0530

    refactor(Font): Font awesome cdn

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,8 @@
-import Head from "next/head";
+import Document from "next/document";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          type="text/javascript"
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?v=2214135"
-          rel="stylesheet"
-        />
-
-        <link
-          type="text/javascript"
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?v=2214135"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
-          crossOrigin="anonymous"
-        />
-      </Head>{" "}
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          type="text/javascript"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?v=2214135"
+          rel="stylesheet"
+        />
+        <link
+          type="text/javascript"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?v=2214135"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
